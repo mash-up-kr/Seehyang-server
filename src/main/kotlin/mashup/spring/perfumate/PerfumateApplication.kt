8c0767b61ee182b2f3737389bbef73cc69af1071
gardenLee <khjvvv7@gmail.com>
2021-09-19 20:00:11 +0900

    init perfumate server

---
+++ src/main/kotlin/mashup/spring/perfumate/PerfumateApplication.kt
@@ -0,0 +1,11 @@
+package mashup.spring.perfumate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PerfumateApplication
+
+fun main(args: Array<String>) {
+	runApplication<PerfumateApplication>(*args)
+}
---
+++ src/test/kotlin/mashup/spring/perfumate/PerfumateApplicationTests.kt
@@ -0,0 +1,13 @@
+package mashup.spring.perfumate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PerfumateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
