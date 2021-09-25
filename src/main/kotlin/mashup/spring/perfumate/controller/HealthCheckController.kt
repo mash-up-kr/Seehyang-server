69f3bbdbb0d44ce0340ca3f39bc665d314544674
gardenLee <khjvvv7@gmail.com>
2021-09-25 17:04:48 +0900

    fix: 헬스체크 엔드포인트 변경

--- src/main/kotlin/mashup/spring/perfumate/controller/HealthCheckController.kt
+++ src/main/kotlin/mashup/spring/perfumate/controller/HealthCheckController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HealthCheckController {
-    @GetMapping("/")
+    @GetMapping("/health")
     fun health() : String {
         return "Perfumate!"
     }
