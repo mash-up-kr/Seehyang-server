c629eef6ff0e112e83631bd71f96214831d03c08
gardenLee <khjvvv7@gmail.com>
2021-09-29 12:21:01 +0900

    fix: 불필요한 코드 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
-	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
 	// library
 
--- src/main/kotlin/mashup/spring/perfumate/PerfumateApplication.kt
+++ src/main/kotlin/mashup/spring/perfumate/PerfumateApplication.kt
@@ -1,11 +1,9 @@
 package mashup.spring.perfumate
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
-@ConfigurationPropertiesScan
 @EnableJpaAuditing
 @SpringBootApplication
 class PerfumateApplication
