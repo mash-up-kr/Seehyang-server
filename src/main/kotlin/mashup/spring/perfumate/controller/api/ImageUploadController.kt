57563577a1afd7783c4869a76af0a801a2d95f33
gardenLee <khjvvv7@gmail.com>
2021-09-29 11:17:41 +0900

    feat: 이미지 업로드 api 이미지 id 반환하도록 변경

--- src/main/kotlin/mashup/spring/perfumate/controller/api/ImageUploadController.kt
+++ src/main/kotlin/mashup/spring/perfumate/controller/api/ImageUploadController.kt
@@ -1,5 +1,7 @@
 package mashup.spring.perfumate.controller.api
 
+import mashup.spring.perfumate.domain.entity.Image
+import mashup.spring.perfumate.repository.ImageRepository
 import mashup.spring.perfumate.service.AwsS3UploadService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -9,11 +11,15 @@ import java.io.IOException
 
 @ApiV1
 class ImageUploadController(
-    private val awsS3UploadService: AwsS3UploadService
+    private val awsS3UploadService: AwsS3UploadService,
+    private val imageRepository: ImageRepository
 ) {
     @PostMapping("/image")
-    @Throws(IOException::class)
-    fun upload(@RequestParam("images") multipartFile: MultipartFile): String? {
-        return awsS3UploadService.upload(multipartFile, "application/post")
+    fun upload(@RequestParam("image") multipartFile: MultipartFile): Long {
+        //TODO: 에러 핸들링 imageUrl NPE?
+        val imageUrl = awsS3UploadService.upload(multipartFile, "application/post")
+        val image = Image(url = imageUrl!!)
+        imageRepository.save(image)
+        return image.id!!
     }
 }
