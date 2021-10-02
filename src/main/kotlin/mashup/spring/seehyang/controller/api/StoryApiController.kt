bfa30e41e0394101569c9173749f2e5ca93ab969
kh0712 <kkkh0712@naver.com>
2021-10-02 18:21:35 +0900

    feat: application runner 완성 및 테스트 코드 작성 (id:1, nickname: adam), (id:2, nickname: eve)

--- src/main/kotlin/mashup/spring/seehyang/controller/api/StoryApiController.kt
+++ src/main/kotlin/mashup/spring/seehyang/controller/api/StoryApiController.kt
@@ -2,6 +2,7 @@ package mashup.spring.seehyang.controller.api
 
 import mashup.spring.seehyang.controller.api.dto.community.StoryCreateRequest
 import mashup.spring.seehyang.controller.api.dto.community.StoryCreateResponse
+import mashup.spring.seehyang.controller.api.dto.community.StoryDto
 import mashup.spring.seehyang.domain.entity.user.User
 import mashup.spring.seehyang.service.StoryService
 import org.springframework.web.bind.annotation.GetMapping
@@ -15,7 +16,9 @@ class StoryApiController(
     @PostMapping("/story")
     fun createStory(createRequest: StoryCreateRequest) : StoryCreateResponse{
         // Todo: User Service 호출
-        
+
+        // Mock up Return
+        return StoryCreateResponse(StoryDto())
     }
 
 }
