be8c96a2a787a2e38e084c5101ce2fe179b59f36
gardenLee <khjvvv7@gmail.com>
2021-10-02 18:23:42 +0900

    feat: story create api

--- src/main/kotlin/mashup/spring/seehyang/service/PerfumeService.kt
+++ src/main/kotlin/mashup/spring/seehyang/service/PerfumeService.kt
@@ -3,11 +3,14 @@ package mashup.spring.seehyang.service
 import mashup.spring.seehyang.domain.entity.perfume.Perfume
 import mashup.spring.seehyang.repository.perfume.PerfumeRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+@Transactional
 @Service
 class PerfumeService(
     private val perfumeRepository: PerfumeRepository
 ) {
+    @Transactional(readOnly = true)
     fun get(id: Long) : Perfume {
         return perfumeRepository.getById(id)
     }
