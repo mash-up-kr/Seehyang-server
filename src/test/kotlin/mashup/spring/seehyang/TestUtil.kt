d795a3daa5d7204ee9b8f29aa38352c50f22abd6
kh0712 <kkkh0712@naver.com>
2021-10-09 18:23:31 +0900

    refactor: Test Util Default Parameter 이용하게끔 수정

--- src/test/kotlin/mashup/spring/seehyang/TestUtil.kt
+++ src/test/kotlin/mashup/spring/seehyang/TestUtil.kt
@@ -1,41 +1,51 @@
 package mashup.spring.seehyang
 
 import mashup.spring.seehyang.domain.entity.Image
+import mashup.spring.seehyang.domain.entity.community.Story
 import mashup.spring.seehyang.domain.entity.perfume.Brand
 import mashup.spring.seehyang.domain.entity.perfume.Gender
 import mashup.spring.seehyang.domain.entity.perfume.Perfume
 import mashup.spring.seehyang.domain.entity.perfume.PerfumeType
 import mashup.spring.seehyang.domain.entity.user.User
 
-fun createTestUser() : User{
-    return User(
-        gender = Gender.BOTH,
-        age = 99,
-        nickname = "test",
-        email = "test@test.com"
-    )
-}
+fun createTestUser(gender: Gender = Gender.BOTH,
+                   age:Short = 99,
+                   nickname: String = "test",
+                   profile:Image = createTestImage())
+= User(gender = gender,
+       age = age,
+       nickname = nickname,
+       email = "${nickname}@test.com",
+       profileImage = profile)
 
-fun createTestBrand() : Brand{
-    return Brand(
-        name ="Chanel",
-        koreanName = "샤넬"
-    )
-}
 
-fun createTestPerfume(brand: Brand) : Perfume{
-    return Perfume(
-        name = "theBestPerfume",
-        koreanName = "최고의향수",
-        type = PerfumeType.EAU_DE,
-        gender = Gender.BOTH,
-        thumbnailUrl = "best/best.jpg",
-        brand = brand
-    )
-}
 
-fun createTestImage(): Image{
-    return Image(
-        url = "image"
-    )
-}
+fun createTestBrand(name:String = "Chanel", koreanName: String = "샤넬")
+= Brand(name = name,
+        koreanName = koreanName)
+
+
+fun createTestPerfume(brand: Brand,
+                      name: String = "theBestPerfume",
+                      koreanName: String ="최고의향수",
+                      type:PerfumeType = PerfumeType.EAU_DE,
+                      gender: Gender = Gender.BOTH,
+                      thumbnailUrl: String = "best/best.jpg",)
+= Perfume(name = name,
+          koreanName = koreanName,
+          type = type,
+          gender = gender,
+          thumbnailUrl = thumbnailUrl,
+          brand = brand)
+
+
+fun createTestImage(url: String = "image")
+= Image(url = url)
+
+
+fun createTestStory(user: User = createTestUser(),
+                    image: Image = createTestImage(),
+                    perfume: Perfume = createTestPerfume(createTestBrand()))
+= Story(user = user,
+        image = image,
+        perfume = perfume)
