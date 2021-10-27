aa43df2b45e1d8e1d4b4e6e90301394b02226143
kh0712 <kkkh0712@naver.com>
2021-10-28 01:20:23 +0900

    feat: 최근 좋아요수 많이 받는 story 가져오는 기능 storylike 리포지토리에 추가

--- src/main/kotlin/mashup/spring/seehyang/repository/community/StoryLikeRepository.kt
+++ src/main/kotlin/mashup/spring/seehyang/repository/community/StoryLikeRepository.kt
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 import java.util.*
 
 @Repository
@@ -14,9 +15,20 @@ interface StoryLikeRepository : JpaRepository<StoryLike, Long> {
 
     @Query("select count(l) " +
             "from StoryLike l " +
-                   "join l.story s " +
+                   "join  l.story s " +
                    "where s.id = :storyId")
     fun countByStoryId(@Param("storyId") storyId: Long) : Long
 
     fun findByUserAndStory(user: User, story: Story) : Optional<StoryLike>
+
+    @Query(nativeQuery = true,
+           value =  "select * " +
+                   "from like_table l join story s on s.id = l.story_id " +
+                   "where l.created_at > :from and l.created_at < :to " +
+                   "group by s.id " +
+                   "order by count(s.id) desc " +
+                   "limit :limit")
+    fun findStoryIdByRecentLike(@Param("from")from: LocalDateTime,
+                                @Param("to") to: LocalDateTime,
+                                @Param("limit") limit: Int): List<Long>
 }
