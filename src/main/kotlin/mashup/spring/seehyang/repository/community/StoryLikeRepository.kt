9b84d2f7f60f476e710850ca50f82a601e8c5c4d
kh0712 <kkkh0712@naver.com>
2021-10-28 16:40:07 +0900

    feat: StoryLike Repo Pageable 적용

--- src/main/kotlin/mashup/spring/seehyang/repository/community/StoryLikeRepository.kt
+++ src/main/kotlin/mashup/spring/seehyang/repository/community/StoryLikeRepository.kt
@@ -3,6 +3,8 @@ package mashup.spring.seehyang.repository.community
 import mashup.spring.seehyang.domain.entity.community.StoryLike
 import mashup.spring.seehyang.domain.entity.community.Story
 import mashup.spring.seehyang.domain.entity.user.User
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
@@ -21,14 +23,12 @@ interface StoryLikeRepository : JpaRepository<StoryLike, Long> {
 
     fun findByUserAndStory(user: User, story: Story) : Optional<StoryLike>
 
-    @Query(nativeQuery = true,
-           value =  "select * " +
-                   "from like_table l join story s on s.id = l.story_id " +
-                   "where l.created_at > :from and l.created_at < :to " +
-                   "group by s.id " +
-                   "order by count(s.id) desc " +
-                   "limit :limit")
+    @Query("select s.id " +
+                   "from StoryLike l join l.story s " +
+                   "where l.createdAt >:from and l.createdAt <:to " +
+                   "group by s " +
+                   "order by count(l) desc")
     fun findStoryIdByRecentLike(@Param("from")from: LocalDateTime,
-                                @Param("to") to: LocalDateTime,
-                                @Param("limit") limit: Int): List<Long>
+                                @Param("to") to: LocalDateTime, pageable: Pageable): List<Long>
+
 }
