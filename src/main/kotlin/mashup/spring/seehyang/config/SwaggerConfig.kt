efae525fb6b65e005079bfe462fe69bd314e36d6
kh0712 <kkkh0712@naver.com>
2021-11-12 17:48:00 +0900

    fix: swagger config auth 헤더 넣을 수 있게끔 수정

--- src/main/kotlin/mashup/spring/seehyang/config/SwaggerConfig.kt
+++ src/main/kotlin/mashup/spring/seehyang/config/SwaggerConfig.kt
@@ -4,9 +4,15 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiKey
+import springfox.documentation.service.AuthorizationScope
+import springfox.documentation.service.SecurityReference
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+import java.util.*
+import java.util.Arrays.*
 
 
 @EnableSwagger2
@@ -15,9 +21,28 @@ class SwaggerConfig {
     @Bean
     fun api(): Docket {
         return Docket(DocumentationType.SWAGGER_2)
+            .securityContexts(listOf(securityContext()))
+            .securitySchemes(listOf(apiKey()))
             .select()
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.ant("/api/v1/**"))
             .build()
+    }
+
+    private fun securityContext(): SecurityContext {
+        return SecurityContext.builder()
+            .securityReferences(defaultAuth())
+            .build()
+    }
+
+    private fun defaultAuth(): List<SecurityReference?>? {
+        val authorizationScope = AuthorizationScope("global", "accessEverything")
+        val authorizationScopes: Array<AuthorizationScope?> = arrayOfNulls<AuthorizationScope>(1)
+        authorizationScopes[0] = authorizationScope
+        return asList(SecurityReference("Authorization", authorizationScopes))
+    }
+
+    private fun apiKey(): ApiKey? {
+        return ApiKey("Authorization", "Authorization", "header")
     }
 }
