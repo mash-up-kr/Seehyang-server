90ee3cf5565da97becd68ff244d505a174007e8d
kh0712 <kkkh0712@naver.com>
2021-11-13 16:02:56 +0900

    fix: Perfume Repository native 쿼리 없이 수정

--- src/main/kotlin/mashup/spring/seehyang/repository/perfume/PerfumeRepository.kt
+++ src/main/kotlin/mashup/spring/seehyang/repository/perfume/PerfumeRepository.kt
@@ -1,8 +1,9 @@
 package mashup.spring.seehyang.repository.perfume
 
-import mashup.spring.seehyang.domain.entity.community.Story
 import mashup.spring.seehyang.domain.entity.perfume.Perfume
-import org.hibernate.annotations.BatchSize
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
@@ -10,45 +11,50 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface PerfumeRepository : JpaRepository<Perfume, Long>{
-    @Query("select p from Perfume p where size(p.stories) >= 5")
+
+    @Query("select p from Perfume p " +
+                   "join fetch p.brand b " +
+                   "where size(p.stories) >= 5")
     fun findByStoryLengthGreaterThan(): List<Perfume>
 
 
-    @Query(nativeQuery = true,
-           value = "select * from perfume " +
-                   "where korean_name like concat('%', :name,'%') and id < :cursor " +
-                   "order by id desc " +
-                   "limit :limit")
-    fun findByKoreanName(@Param("name") name: String, @Param("cursor") cursor: Long, @Param("limit") limit: Int): List<Perfume>
+    @Query("select p from Perfume p " +
+                   "join fetch p.brand b " +
+                   "where p.koreanName like %:name% and p.id < :cursor " +
+                   "order by p.id desc")
+    fun findByKoreanName(@Param("name") name: String, @Param("cursor") cursor: Long, pageable: Pageable): List<Perfume>
 
-    @Query(nativeQuery = true,
-           value = "select * from perfume " +
-                   "where upper(name) like concat('%', upper(:name),'%') and id < :cursor " +
-                   "order by id desc " +
-                   "limit :limit")
-    fun findByEngName(@Param("name") name: String, @Param("cursor") cursor: Long, @Param("limit") limit: Int): List<Perfume>
+    @Query("select p from Perfume p " +
+                   "join fetch p.brand b " +
+                   "where upper(p.name) like upper(concat('%',:name,'%')) and p.id < :cursor " +
+                   "order by p.id desc")
+    fun findByEngName(@Param("name") name: String, @Param("cursor") cursor: Long, pageable: Pageable): List<Perfume>
 
 
-    @Query(nativeQuery = true,
-           value = "select * from perfume " +
-                   "where  (like_count < :likeCursor or (like_count = :like_count and id < :idCursor)) " +
-                   "order by like_count desc, id desc " +
-                   "limit :limit")
-    fun findSteadyPerfume(@Param("idCursor") idCursor: Long, @Param("likeCursor") likeCursor: Int, @Param("limit") limit: Int): List<Perfume>
+    @Query("select p from Perfume p " +
+                   "join fetch p.brand b " +
+                   "where (p.likeCount < :likeCursor or (p.likeCount = :like_count and p.id < :idCursor)) " +
+                   "order by p.likeCount desc, p.id desc")
+    fun findSteadyPerfume(@Param("idCursor") idCursor: Long, @Param("likeCursor") likeCursor: Int, pageable: Pageable): List<Perfume>
 
-    @Query(nativeQuery = true,
-           value = "select * from perfume " +
-                   "where  id < :idCursor " +
-                   "order by id desc "+
-                   "limit :limit")
-    fun findSteadyPerfume(@Param("idCursor") idCursor: Long, @Param("limit") limit: Int): List<Perfume>
+    @Query("select p from Perfume p " +
+                   "join fetch p.brand b " +
+                   "where p.id < :idCursor " +
+                   "order by p.id desc")
+    fun findSteadyPerfume(@Param("idCursor") idCursor: Long, pageable: Pageable): List<Perfume>
 
+    @EntityGraph(attributePaths = ["brand"])
     fun findTop10ByOrderByLikeCountDesc(): List<Perfume>
+    @EntityGraph(attributePaths = ["brand"])
     fun findTop6ByOrderByLikeCountDescIdDesc(): List<Perfume>
+    @EntityGraph(attributePaths = ["brand"])
     fun findTop10ByKoreanNameContainsOrderByIdDesc(name: String): List<Perfume>
+    @EntityGraph(attributePaths = ["brand"])
     fun findTop10ByNameContainsIgnoreCaseOrderByIdDesc(name: String): List<Perfume>
 
 
-    @Query("select p from Perfume p join fetch p.brand b where p.id in :ids")
+    @Query("select p from Perfume p " +
+                   "join fetch p.brand b " +
+                   "where p.id in :ids")
     fun findByIds(@Param("ids") storyIds: List<Long>): List<Perfume>
 }
